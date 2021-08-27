0a137a1003e1dae05e75ddfa108551338776a379
Chiro <sachinsinghroxx@gmail.com>
2021-08-27 17:43:23 +0530

    finsihed front end prototype

--- frontend-website/components/Cards.tsx
+++ frontend-website/components/Cards.tsx
@@ -1,15 +1,50 @@
 import { Card, Button } from "react-bootstrap";
+import { useState } from "react";
 
-const Cards = () => {
+type RecipeProps = {
+  recipe: string | null;
+};
+
+const Cards: React.FC<RecipeProps> = (props) => {
+  const [recipeTitle, setRecipeTitle] = useState("Recipe Name will be here");
+  const [recipeIngred, setRecipeIngred] = useState(
+    "Recipe Ingredients will be here"
+  );
+
+  const [recipeInstructions, setRecipeInstructions] = useState(
+    "Recipe Instructions will be here"
+  );
+  let tempArr: string[] = [];
+  if (props.recipe) {
+    console.log("something");
+    tempArr = props.recipe.split("^^^^");
+    console.log(tempArr);
+    if (recipeTitle !== tempArr[0]) {
+      setRecipeTitle(tempArr[0]);
+      setRecipeIngred(tempArr[1]);
+      setRecipeInstructions(tempArr[2]);
+    }
+  }
+
+  //   const formatString = (recipe: string | null) => {
+  //     if (recipe === null) return;
+  //     setRecipeTitle(tempArr[0]);
+  //     setRecipeIngred(tempArr[1]);
+  //     setRecipeInstructions(tempArr[2]);
+  //   };
+
   return (
     <Card style={{ width: "18rem" }}>
       <Card.Img variant="top" src="https://picsum.photos/200/300" />
       <Card.Body>
-        <Card.Title>Card Title</Card.Title>
+        <Card.Title>{recipeTitle}</Card.Title>
         <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card&apos;s content.
+          { recipeIngred.split(";").map((ingred) => (
+            <li>{ingred}</li>
+          ))}
         </Card.Text>
+        <br />
+        <Card.Text>{recipeInstructions}</Card.Text>
         <Button variant="primary">Go somewhere</Button>
       </Card.Body>
     </Card>
